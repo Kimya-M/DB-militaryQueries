cc290c93a68e7610ad1dbe0d6032bf7e85739994
Mohadese Akhoondy <m.akhoondy1381@gmail.com>
2024-11-20 20:24:32 +0330

    updates Q11 & Q5

--- Queries/Q11.sql
+++ Queries/Q11.sql
@@ -1,4 +1,4 @@
 SELECT e.Quantity
 FROM Equipments e JOIN Missions m on e.Base_ID = m.Base_ID
-JOIN Units u on e.Unit_ID_Uses = u.Unit_ID
+JOIN Units u on e.Unit_ID_Uses = u.Unit_ID AND e.Base_ID = u.Base_ID
 where m.Units_Lost >= 1 and u.Unit_Type = 'Helicopter';
--- Queries/Q5.sql
+++ Queries/Q5.sql
@@ -1,4 +1,4 @@
 SELECT e.Equipment_Model,b.Base_Name
 FROM Equipments e JOIN Bases b ON e.Base_ID = b.Base_ID JOIN Units u on
-u.Unit_ID = e.Unit_ID_Uses
+u.Unit_ID = e.Unit_ID_Uses AND u.Base_ID = e.Base_ID
 WHERE u.Unit_Type = 'Tank';
