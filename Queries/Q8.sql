a8038744e4b5f57b73be82faed2b153798d41a84
Kimya Mirmoghtadaei <kimyamirmoghtadaei@gmail.com>
2024-11-25 16:25:16 +0330

    fixed the miserable query i've written

--- Queries/Q8.sql
+++ Queries/Q8.sql
@@ -1,5 +1,5 @@
 select Soldiers.Soldier_ID, Soldiers.Base_ID, Soldiers.Soldier_Name, Soldiers.Rank, Soldiers.Join_Date, Soldiers.Age, Soldiers.Mission_Number, Reports.Report_ID
 from Soldiers
-NATURAL join Reports
-NATURAL join Missions
+join Reports on Soldiers.Soldier_ID = Reports.Reporter_ID
+join Missions on Missions.Mission_ID = Reports.Mission_ID
 where Missions.Target like 'Terrorist%';
