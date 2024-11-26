79148c3348bac3361696f5e8a797995b31b7c21a
Mohadese Akhoondy <m.akhoondy1381@gmail.com>
2024-11-26 12:41:54 +0330

    updates EMT4

--- Queries/EMT4.sql
+++ Queries/EMT4.sql
@@ -3,7 +3,7 @@ FROM Equipments e
 JOIN Units u ON e.Unit_ID_Uses = u.Unit_ID   
 JOIN Missions m ON m.Base_ID = u.Base_ID   
 JOIN Soldiers s ON s.Base_ID = m.Base_ID   
-JOIN Bases b ON b.Base_ID = s.Base_ID   
+JOIN Bases b ON b.Base_ID = e.Base_ID   
 WHERE u.Missions_Number < 11   
   AND m.Target LIKE 'Enemy%'   
   AND s.Mission_Number >= 3   
