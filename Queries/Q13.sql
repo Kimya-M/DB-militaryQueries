254a2511ab723d10e7187d44196617332358b73b
Kimya Mirmoghtadaei <kimyamirmoghtadaei@gmail.com>
2024-11-28 17:21:15 +0330

    Q13 fixed

--- Queries/Q13.sql
+++ Queries/Q13.sql
@@ -1,4 +1,6 @@
 SELECT s.soldier_name, u.unit_type, m.mission_name
-FROM Soldiers s JOIN Units u ON s.Base_ID = u.Base_ID
-JOIN Missions m on m.Base_ID = s.Base_ID
+FROM Soldiers s 
+join Tasks t on t.Soldier_ID = s.Soldier_ID
+join Missions m on t.Mission_ID = m.Mission_ID
+join Units u on u.Unit_ID = t.Unit_ID
 WHERE s.Age < 28
