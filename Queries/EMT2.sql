0925c01d1370aeec3359f2eb412379b768f2e1c9
Kimya Mirmoghtadaei <kimyamirmoghtadaei@gmail.com>
2024-11-28 18:04:08 +0330

    EMTs fixed except 4

--- Queries/EMT2.sql
+++ Queries/EMT2.sql
@@ -1,7 +1,7 @@
 SELECT t.Task_Name, t.Description,t.Status
 FROM Tasks t JOIN Soldiers s on t.Soldier_ID = s.Soldier_ID 
-JOIN Units u on u.Base_ID = s.Base_ID
-JOIN Equipments e on e.Unit_ID_Uses = u.Unit_ID
+JOIN Units u on u.Unit_ID = t.Unit_ID
+JOIN Equipments e on e.Unit_ID_Uses = u.Unit_ID 
 where s.Age > 27 
 	and u.Missions_Number > 14 
 	and e.Quantity < 60
--- Queries/EMT3.sql
+++ Queries/EMT3.sql
@@ -1,5 +1,6 @@
 SELECT b.Base_Name
  From Bases b JOIN Soldiers s on b.Base_ID = s.Base_ID 
- JOIN Missions m on m.Base_ID = s.Base_ID
- JOIN Units u on u.Base_ID = m.Base_ID
+ join Tasks t on t.Soldier_ID = s.Soldier_ID
+ JOIN Missions m on m.Mission_ID = t.Mission_ID
+ JOIN Units u on u.Unit_ID = t.Unit_ID
  WHERE s.Rank = 'Captain' and m.Soldiers_Lost > 3 and u.Unit_Type = 'Tank'
