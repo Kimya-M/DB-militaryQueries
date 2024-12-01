38ddb1092278d3fb59ce70fcbedeb428f4b0f37c
Mohadese Akhoondy <m.akhoondy1381@gmail.com>
2024-12-01 17:52:55 +0330

    updates a line of emt4

--- Queries/EMT4.sql
+++ Queries/EMT4.sql
@@ -1,8 +1,9 @@
 SELECT e.Equipment_Type, e.Equipment_Model
 FROM Equipments e   
-JOIN Units u ON e.Unit_ID_Uses = u.Unit_ID   
-JOIN Missions m ON m.Base_ID = u.Base_ID   
-JOIN Soldiers s ON s.Base_ID = e.Base_ID   
+JOIN Units u ON e.Unit_ID_Uses = u.Unit_ID  
+JOIN Tasks t ON u.Unit_ID = t.Task_ID
+JOIN Missions m ON m.Mission_ID = t.Mission_ID   
+JOIN Soldiers s ON s.Soldier_ID = t.Soldier_ID   
 JOIN Bases b ON b.Base_ID = e.Base_ID   
 WHERE u.Missions_Number < 11   
   AND m.Target LIKE 'Enemy%'   
