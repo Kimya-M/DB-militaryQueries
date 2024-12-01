4051d7c1ca522eb291d895cf1da17e1a635de7f0
Mohadese Akhoondy <m.akhoondy1381@gmail.com>
2024-12-01 18:22:47 +0330

    updates Q10

--- Queries/Q10.sql
+++ Queries/Q10.sql
@@ -1,7 +1,8 @@
 SELECT DISTINCT Attendance.date
 from Attendance
 where Attendance.Status = false
-and Attendance.Soldier_ID in (SELECT DISTINCT Soldiers.soldier_ID
+and EXISTS (SELECT DISTINCT Soldiers.soldier_ID
                  			  from Soldiers
-                			  join Missions on Missions.Base_ID = Soldiers.Base_ID
-                			  where Missions.Soldiers_Lost > 3);
+                              JOIN Tasks on Soldiers.Soldier_ID = Tasks.Soldier_ID
+                			  join Missions on Missions.Mission_ID = Tasks.Mission_ID
+                			  where Missions.Soldiers_Lost > 3 and Attendance.Soldier_ID = Soldiers.Soldier_ID);
