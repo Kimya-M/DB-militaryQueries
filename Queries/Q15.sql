5cb298a91d15efa9e2e4e8923099ac18910239ce
Mohadese Akhoondy <m.akhoondy1381@gmail.com>
2024-12-01 18:40:59 +0330

    updates Q15

--- Queries/Q15.sql
+++ Queries/Q15.sql
@@ -1,8 +1,8 @@
-SELECT COUNT(DISTINCT s.Soldier_ID) AS Total_Soldiers
-FROM Soldiers s
-WHERE s.Base_ID IN (
-    SELECT DISTINCT s.Base_ID
-    FROM Soldiers s
-    JOIN Tasks t ON s.Soldier_ID = t.Soldier_ID
-    WHERE t.Status = 'In Progress'
-);
+SELECT SUM(Bases.soldier_number) AS Total_Soldiers
+FROM Bases JOIN Soldiers on Bases.Base_ID = Soldiers.Base_ID
+where Soldiers.Soldier_ID in 
+(
+  SELECT Soldiers.Soldier_ID
+  FROM Soldiers JOIN Tasks on Soldiers.Soldier_ID = Tasks.Soldier_ID
+  WHERE Tasks.Status = 'In Progress'
+)
